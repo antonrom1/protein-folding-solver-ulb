152d624ace6834bd9624daa3039d5501a640a51e
anton <anton.romanova@ulb.be>
2022-11-30 12:14:40 +0100

    Removed debug code

--- src/folding.py
+++ src/folding.py
@@ -210,7 +210,3 @@ if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
-
-    s = sequence.Sequence('1110101101')
-    solver = FoldingSolver(s)
-    print(solver.solve(7).solution)
